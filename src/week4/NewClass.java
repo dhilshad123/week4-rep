30a95e2fee1624a52ebec474633f0e911cec61d6
dhilshad123 <dilshadyouseph944@gmail.com>
2022-02-08 06:37:17 -0500

    added a new variable z

--- src/week4/NewClass.java
+++ src/week4/NewClass.java
@@ -3,9 +3,10 @@ public class NewClass {
     int x ;
     int y ;
 
-    public NewClass( int x , int y){
+    public NewClass( int x , int y,int z){
         this.x = x;
         this.y = y;
+        this.z=z;
     }
     
 }
